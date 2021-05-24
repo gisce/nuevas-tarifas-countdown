d4e050491fcd7bc120f93fcfe1b62bbf096877ee
Eduard Carrerars <ecarreras@gisce.net>
2021-05-24 11:45:43 +0200

    Spanish

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ const secondsToDhms = (value) => {
   const m = Math.floor((seconds % 3600) / 60);
   const s = Math.floor(seconds % 60);
 
-  const dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
-  const hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-  const mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-  const sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+  const dDisplay = d > 0 ? d + (d == 1 ? " día, " : " días, ") : "";
+  const hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " horas, ") : "";
+  const mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " minutos, ") : "";
+  const sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " segundos") : "";
   return dDisplay + hDisplay + mDisplay + sDisplay;
 }
 
