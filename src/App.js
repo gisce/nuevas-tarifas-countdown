297173b111a05a69f3e03bba2f35287849513f71
Eduard Carrerars <ecarreras@gisce.net>
2021-05-24 11:32:16 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import { CountdownCircleTimer } from "react-countdown-circle-timer";
+
+import "./styles.css";
+
+const secondsToDhms = (value) => {
+  const seconds = Number(value);
+  const d = Math.floor(seconds / (3600 * 24));
+  const h = Math.floor((seconds % (3600 * 24)) / 3600);
+  const m = Math.floor((seconds % 3600) / 60);
+  const s = Math.floor(seconds % 60);
+
+  const dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+  const hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+  const mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+  const sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+  return dDisplay + hDisplay + mDisplay + sDisplay;
+}
+
+const renderTime = ({ remainingTime }) => {
+  if (remainingTime === 0) {
+    return (
+      <div className="timer">
+        <span role="img">🎉</span> Ya ha llegado el momento!
+      </div>
+    );
+  }
+
+  return (
+    <div className="timer">
+      <div className="value">{secondsToDhms(remainingTime)}</div>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Aplicación de las nuevas tarifas</h1>
+      <div className="timer-wrapper">
+        <CountdownCircleTimer
+          isPlaying
+          duration={(new Date(2021, 5, 1) - new Date()) / 1000}
+          colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+          onComplete={() => [true, 1000]}
+          size={500}
         >
-          Learn React
-        </a>
-      </header>
+          {renderTime}
+        </CountdownCircleTimer>
+      </div>
     </div>
   );
 }
