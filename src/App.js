cb71bcefb83cbe85cf9a5d7a48397918647f06dc
Eduard Carrerars <ecarreras@gisce.net>
2021-06-01 10:23:59 +0200

    pues si que entraron

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const secondsToDhms = (value) => {
   const hDisplay = h > 0 ? h + (h == 1 ? " hora, " : " horas ") : "";
   const mDisplay = m > 0 ? m + (m == 1 ? " minuto, " : " minutos ") : "";
   const sDisplay = s > 0 ? s + (s == 1 ? " segundo" : " segundos") : "";
-  return {days: dDisplay, hours: hDisplay, minutes: mDisplay, seconds: sDisplay};
+  return { days: dDisplay, hours: hDisplay, minutes: mDisplay, seconds: sDisplay };
 }
 
 const renderTime = ({ remainingTime }) => {
@@ -25,12 +25,12 @@ const renderTime = ({ remainingTime }) => {
     );
   }
 
-  const {days, hours, minutes, seconds} = secondsToDhms(remainingTime);
+  const { days, hours, minutes, seconds } = secondsToDhms(remainingTime);
 
   return (
     <div className="timer">
       <div className="value">
-        {days}<br/>{hours}<br/>{minutes}<br/>{seconds}
+        {days}<br />{hours}<br />{minutes}<br />{seconds}
       </div>
     </div>
   );
@@ -38,25 +38,31 @@ const renderTime = ({ remainingTime }) => {
 
 function App() {
   const endDate = new Date(2021, 5, 1);
+  const now = new Date();
   return (
     <div className="App">
       <h1>Aplicación de las nuevas tarifas</h1>
-      <div className="timer-wrapper">
-        <CountdownCircleTimer
-          isPlaying
-          duration={(endDate - new Date(2021, 4, 24)) / 1000}
-          initialRemainingTime={(endDate - new Date()) / 1000}
-          colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
-          onComplete={() => [true, 1000]}
-          strokeWidth={26}
-          size={300}
-        >
-          {renderTime}
-        </CountdownCircleTimer>
-      </div>
-      <h1><span>🙅‍♂️</span> #novanaentrar</h1>
-      <h1><span>🕹️</span> #estonoesunjuego</h1>
-      <h1><span>🆙</span> #updateall</h1>
+      {now < endDate &&
+        <div className="timer-wrapper">
+          <CountdownCircleTimer
+            isPlaying
+            duration={(endDate - new Date(2021, 4, 24)) / 1000}
+            initialRemainingTime={(endDate - now) / 1000}
+            colors={[["#004777", 0.33], ["#F7B801", 0.33], ["#A30000"]]}
+            onComplete={() => [true, 1000]}
+            strokeWidth={26}
+            size={300}
+          >
+            {renderTime}
+          </CountdownCircleTimer>
+        </div>
+      }
+      {now > endDate &&
+        <h1><span>🤦‍♂️</span>Pues sí que entraron... que la fuerza te acompañe</h1>
+      }
+      <h2><span>🙅‍♂️</span> #novanaentrar</h2>
+      <h2><span>🕹️</span> #estonoesunjuego</h2>
+      <h2><span>🆙</span> #updateall</h2>
     </div>
   );
 }
